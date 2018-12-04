77becb9bc97a82a6a5b7c54f14f9e345f758783f
Patrycja Gorczynska <patrycjagorczynska@Patrycjas-MacBook-Pro.local>
2018-12-03 22:06:00 -0700

    psuedo-coded

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+//Create a game that can ask and answer questions
+
+/* Questions:
+1. In Aladdin, what is the name of Jasmine’s pet tiger?
+Genie, Thumper, Rajah
+
+2.In the Lion King, where does Mufasa and his family live?
+Pride Rock, Zootopia, Magic Mountain
+
+3. What time does the royal ball start in Cinderella?
+12am, 8pm, 3pm
+
+4. Which princess attempts to disguise herself as a man?
+Mulan, Pocahontas, Rapunzel
+
+5. Who was the first Disney princess?
+Snow White, Aurora, Cinderella
+
+
+
+//have a page that says Start, once you click start, you will be directed to the game
+
+//Once you press start the timer starts to tick down and you start answering trivia questions
+//You can only provide one answer per question
+
+//Once the timer goes off, it switches to another page that says All done with the :
+// :Correct Answers :Incorrect Answers :Unanswered
+
+//Or if you're done answering ?'s before the timer clock is done, it will bring you to the same page as if the timer were to go off. 
+
+*/
