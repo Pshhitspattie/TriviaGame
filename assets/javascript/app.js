22fc58995966e9939b7d2ebefed6c12ad8b4dc06
Patrycja Gorczynska <patrycjagorczynska@Patrycjas-MacBook-Pro.local>
2018-12-09 15:12:18 -0700

    Finished some JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,47 +46,133 @@ var trivia = {
   timerOn: false,
   timerId: '',
 
+
+  questions: {
+    q1: "In Aladdin, what is the name of Jasmine’s pet tiger?",
+    q2: "In the Lion King, where does Mufasa and his family live?",
+    q3: "What time does the royal ball start in Cinderella?",
+    q4: 'Which princess attempts to disguise herself as a man?',
+    q5: 'Who was the first Disney princess?'
+  },
+  choices: {
+    q1: ['Genie', 'Thumper', 'Rajah'],
+    q2: ['Pride Rock', 'Zootopia', 'Magic Mountain'],
+    q3: ['12am', '8pm', '3pm'],
+    q4: ['Mulan', 'Pocahontas', 'Rapunzel'],
+    q5: ['Snow White', 'Aurora', 'Cinderella']
+  },
+  answers: {
+    q1: 'Rajah',
+    q2: 'Pride Rock',
+    q3: '8pm',
+    q4: 'Mulan',
+    q5: 'Snow White'
+  },
+   
+ startGame: function(){
+   //restarts
+   trivia.currentSet = 0;
+   trivia.correct = 0;
+   trivia.incorrect = 0;
+   trivia.unanswered = 0;
+   clearTimeout(trivia.timerId);
+
+   $('#game').show();
+
+   $('#results').html('');
+
+   $('#timer').text(trivia.timer);
+
+   $('#start').hide();
+
+   $('#remaining-time').show();
+
+   trivia.nextQuestion();
+
+ },
+
+ nextQuestion : function(){
+
+
+  trivia.timer = 10;
+  $('#timer').removeClass('last-seconds');
+  $('#timer').text(trivia.timer);
+
+  if(!trivia.timerOn) {
+    trivia.timerId = setInterval(trivia.timerRunning, 1000);
+  }
+
+var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+$('#question').text(questionContent);
+
+questionOptions = Object.values(trivia.options) [trivia.currentSet];
+
+$.each(questionOptions, function(index, key){
+  $('options').append($('<button class=option btn btn-info btn-lg'>+key+'</button>'));
+})
+
+ },
+
+ timerRunning : function() {
+
+  if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+    $('#timer').text(trivia.timer);
+    trivia.timer--;
+    if(trivia.timer === 4){
+      $('#timer').addClass('last-seconds');
+    }
+  }
+
+  else if(trivia.timer === -1) {
+    trivia.unanswered++;
+    trivia.result = false;
+    clearInterval(trivia.timerId);
+    resultId = setTimeout(trivia.guessResult, 1000);
+    $('#results').html('<h3> Shoot! You are out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] + '</h3>');
+  }
+
+  else if(trivia.currentSet === Object.keys(trivia.questions).length){
+
+    $('#results')
+    .html('<h3> It has been magical of you to play!</h3>'+
+    '<p>Correct: ' + trivia.correct + '</p>'+
+    '<p>Incorrect: '+ trivia.incorrect + '</p>' +
+    '<Unanswered: '+ trivia.unanswered +'</p>' +
+    '<p> Come play again soon!</p>' );
+
+    $('#game').hide();
+
+    $('#start').show();
+  
+  }
+
+ },
+
+ quessChecker : function() {
+
+  var resultId;
+
+  var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+
+  if($(this).text() === currentAnswer){
+
+    trivia.correct++;
+    clearInterval(trivia.timerId);
+    resultId = setTimeout(trivia.guessResult, 1000);
+    $('#results').html('<h3> Correct Answer!</h3>');
+  }
+
+ },
+
+ guessResult : function() {
+
+  trivia.crrentSet++;
+
+  $('.option').remove();
+  $('#results h3').remove();
+
+  trivia.nextQuestion();
+ }
+
+
 }
-  var questions = [{
-    question: "In Aladdin, what is the name of Jasmine’s pet tiger?",
-    answers: ["Genie", "Thumper", "Rajah"],
-    correctAnswer: "Rajah",
-    image:"assets/images/Rajah.gif"
-  }, {
-    question: "In the Lion King, where does Mufasa and his family live?",
-    answers: ["Pride Rock", "Zootopia", "Magic Mountain"],
-    correctAnswer: "Pride Rock",
-    image:"assets/images/Simba.gif"
-  }, {
-    question: "What time does the royal ball start in Cinderella?",
-    answers: ["12am", "8pm", "3pm"],
-    correctAnswer: "8pm",
-    image:"assets/images/Cinderella.gif"
-  }, {
-    question: 'Which princess attempts to disguise herself as a man?',
-    answers: ["Mulan", "Pocahontas", "Rapunzel"],
-    correctAnswer: "Mulan",
-    image:"assets/images/mulan.gif"
-  }, {
-    question: 'Which popular Disney movie featured the song, "Circle of Life"?',
-    answers: ["Aladdin", "Hercules", "The Lion King"],
-    correctAnswer: "The Lion King",
-    image:"assets/images/Simba.gif"
-  }, {
-    question: 'Who was the first Disney princess?', 
-    answers: ["Snow White", "Aurora", "Cinderella"],
-    correctAnswer: "Snow White",
-    image:"assets/images/snowwhite.gif"
- 
-  }]
-
-
-  
-  
-  
-
-  
-  
-
-  
-  
